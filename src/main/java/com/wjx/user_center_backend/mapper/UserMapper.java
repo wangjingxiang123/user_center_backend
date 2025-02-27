05d87100155bef8828a08818a3124360bff39ed5
wangjingxiang123 <1499655471@qq.com>
2025-02-27 21:52:52 +0800

    初始化

---
+++ src/main/java/com/wjx/user_center_backend/mapper/UserMapper.java
@@ -0,0 +1,9 @@
+package com.wjx.user_center_backend.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.wjx.usercenter.model.User;
+
+public interface UserMapper extends BaseMapper<User> {
+
+
+}
